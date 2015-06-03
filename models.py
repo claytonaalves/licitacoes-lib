bba67b860c1f83f7e37e2a41488ee8db304a4706
Clayton A. Alves <clayton.aa@gmail.com>
2015-06-03 08:10:37 -0300

    Correção na filtragem e apresentação das datas na view

--- models.py
+++ models.py
@@ -3,6 +3,10 @@ from peewee import RawQuery
 
 database = MySQLDatabase('licitacoes', **{'user': 'root'})
 
+def formata_data(data):
+    dia, mes, ano = data.split('/')
+    return "%s-%s-%s" % (ano, mes, dia)
+
 class UnknownField(object):
     pass
 
@@ -64,18 +68,23 @@ class Licitacao(BaseModel):
             select = select.where(Licitacao.uf==kwargs['estado'])
         if kwargs['modalidade']!='todas':
             select = select.where(Licitacao.modalidade==kwargs['modalidade'])
-        if kwargs['credenciamento_inicio'] and kwargs['credenciamento_fim']:
-            select = select.where(Licitacao.prazo_credenciamento.between(
-                                    formata_data(kwargs['credenciamento_inicio']), 
-                                    formata_data(kwargs['credenciamento_fim'])))
+        if kwargs['credenciamento_inicio']:
+            data1 = formata_data(kwargs['credenciamento_inicio'])+' 00:00:00'
+            data2 = formata_data(kwargs['credenciamento_inicio'])+' 23:59:59'
+            select = select.where(Licitacao.prazo_credenciamento.between(data1, data2)) 
+        if kwargs['credenciamento_fim']:
+            data1 = formata_data(kwargs['credenciamento_fim'])+' 00:00:00'
+            data2 = formata_data(kwargs['credenciamento_fim'])+' 23:59:59'
+            select = select.where(Licitacao.prazo_proposta.between(data1, data2)) 
         if kwargs['cotacao_inicio'] and kwargs['cotacao_fim']:
             select = select.where(
             (Licitacao.cotacao_inicio>=formata_data(kwargs['cotacao_inicio'])+' 00:00:00') &
             (Licitacao.cotacao_fim<=formata_data(kwargs['cotacao_fim'])+' 23:59:59') 
             )
         if kwargs['objeto']:
-            select = select.where(SQL("match (objeto) against ('{}')".format(kwargs['objeto'])))
+            select = select.where(SQL("match (objeto) against ('{0}')".format(kwargs['objeto'])))
 
         select = select.limit(30)
+        print select
         return select
 
