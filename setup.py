64278d83a05fe9f285f95d2355bd948c6b2636a5
Clayton A. Alves <clayton.aa@gmail.com>
2015-10-15 21:23:52 -0400

    Adicionado arquivo setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# coding: utf8
+from setuptools import setup
+
+setup(name='licitacoes',
+      version='0.1',
+      description='Parser de Licitações',
+      url='https://github.com/claytonaalves/licitacoes-lib',
+      author='Clayton A. Alves',
+      author_email='clayton.aa@gmail.com',
+      license='MIT',
+      packages=['licitacoes'],
+      zip_safe=False)
